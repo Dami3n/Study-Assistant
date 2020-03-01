6c463209177a82c5428b666a274e1033c787201e
Damien Lo <damienlo@cdnis.edu.hk>
2020-03-01 19:28:08 +0800

    Initial Commit

---
+++ Study-Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Study-Assistant
+//
+//  Created by Damien Lo on 1/3/2020.
+//  Copyright © 2020 Damien Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Study-AssistantTests/Study_AssistantTests.swift
@@ -0,0 +1,34 @@
+//
+//  Study_AssistantTests.swift
+//  Study-AssistantTests
+//
+//  Created by Damien Lo on 1/3/2020.
+//  Copyright © 2020 Damien Lo. All rights reserved.
+//
+
+import XCTest
+@testable import Study_Assistant
+
+class Study_AssistantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
