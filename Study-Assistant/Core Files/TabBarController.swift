c76489eaeadf32da4b0451135bd15db9d0004e14
Damien Lo <damienlo@cdnis.edu.hk>
2020-03-01 20:47:37 +0800

    Initial Commit Configure Entrance View Configure UI View Controllers Configure Swift Files

---
+++ Study-Assistant/Core Files/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  Study-Assistant
+//
+//  Created by Damien Lo on 1/3/2020.
+//  Copyright © 2020 Damien Lo. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
